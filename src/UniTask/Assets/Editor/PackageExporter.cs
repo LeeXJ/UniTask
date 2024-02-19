a8e6eb5c4c6c24281025316bb83eefc53d6e2181
LeeXJ <872846303@QQ.COM>
2024-02-19 04:03:57 +0000

    t

--- src/UniTask/Assets/Editor/PackageExporter.cs
+++ src/UniTask/Assets/Editor/PackageExporter.cs
@@ -35,32 +35,44 @@ public static class PackageExporter
 
     static string GetVersion(string root)
     {
+        // 从环境变量中获取Unity包版本
         var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
+        // 组合package.json文件的路径
         var versionJson = Path.Combine(Application.dataPath, root, "package.json");
 
+        // 检查package.json文件是否存在
         if (File.Exists(versionJson))
         {
+            // 读取package.json文件中的内容，并将其解析为Version对象
             var v = JsonUtility.FromJson<Version>(File.ReadAllText(versionJson));
 
+            // 如果环境变量中的版本不为空
             if (!string.IsNullOrEmpty(version))
             {
+                // 如果package.json中的版本与环境变量中的版本不匹配
                 if (v.version != version)
                 {
+                    // 构建错误消息，指出版本不匹配
                     var msg = $"package.json and env version are mismatched. UNITY_PACKAGE_VERSION:{version}, package.json:{v.version}";
 
+                    // 如果应用程序正在批处理模式下运行
                     if (Application.isBatchMode)
                     {
+                        // 将错误消息打印到控制台并退出应用程序
                         Console.WriteLine(msg);
                         Application.Quit(1);
                     }
 
+                    // 抛出异常，指示package.json和环境变量中的版本不匹配
                     throw new Exception("package.json and env version are mismatched.");
                 }
             }
 
+            // 更新版本为从package.json文件中获取的版本
             version = v.version;
         }
 
+        // 返回获取到的版本信息
         return version;
     }
 
