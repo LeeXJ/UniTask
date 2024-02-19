b57e0ab8d5bc6843e628a22ffcf63c4aabbe4207
LeeXJ <872846303@QQ.COM>
2024-02-19 03:09:07 +0000

    t

--- src/UniTask/Assets/Editor/EditorRunnerChecker.cs
+++ src/UniTask/Assets/Editor/EditorRunnerChecker.cs
@@ -18,15 +18,16 @@ public static class EditorRunnerChecker
 
     static async UniTaskVoid RunCore()
     {
-        Debug.Log("Start");
+        Debug.Log("Start"); // 打印 "Start" 到 Unity 控制台
 
+        // 下面的代码被注释掉了，因此不会被执行
         //var r = await UnityWebRequest.Get("https://bing.com/").SendWebRequest().ToUniTask();
         //Debug.Log(r.downloadHandler.text.Substring(0, 100));
-        //await UniTask.Yield();
 
+        // 在当前帧结束前等待一段时间，然后继续执行
         await UniTask.DelayFrame(30);
 
-        Debug.Log("End");
+        Debug.Log("End"); // 打印 "End" 到 Unity 控制台
     }
 }
 
